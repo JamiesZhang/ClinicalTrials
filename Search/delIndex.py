80356e723c30bf1561c586046d35a261d7f38d75
ZhangJiahua <zhangjiahua@ict.ac.cn>
2018-12-27 20:37:07 +0800

    debug delIndex.py

--- Search/delIndex.py
+++ Search/delIndex.py
@@ -2,5 +2,5 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch([{'host':'localhost', 'port' : 9200}])
 
-es.indices.delete(index='clinicaltrials_bm25', doc_type='trial')
-es.indices.delete(index='clinicaltrials_tfidf', doc_type='trial')
+es.indices.delete(index='clinicaltrials_bm25')
+es.indices.delete(index='clinicaltrials_tfidf')
