e4f0db79061ce123630d002169c47cf6bd0ec2bb
ZhangJiahua <zhangjiahua@ict.ac.cn>
2018-12-30 00:41:42 +0800

    add if

--- Search/delIndex.py
+++ Search/delIndex.py
@@ -2,6 +2,16 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch([{'host':'localhost', 'port' : 9200}])
 
-es.indices.delete(index='clinicaltrials_bm25')
-es.indices.delete(index='clinicaltrials_tfidf')
-print('delete successful！')
+if not es.indices.exists(index='clinicaltrials_bm25'):
+    print("The index \"clinicaltrials_bm25\" does not exist, you don't need delete it")
+else:
+    es.indices.delete(index='clinicaltrials_bm25')
+    print('clinicaltrials_bm25 delete successful！')
+
+print()
+
+if not es.indices.exists(index='clinicaltrials_bm25'):
+    print("The index \"clinicaltrials_tfidf\" does not exist, you don't need delete it")
+else:
+    es.indices.delete(index='clinicaltrials_tfidf')
+    print('clinicaltrials_tfidf delete successful！')
