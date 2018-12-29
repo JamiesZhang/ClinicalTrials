143bee8666bdcb891b0fed912dee943016cbd4c9
ZhangJiahua <zhangjiahua@ict.ac.cn>
2018-12-30 04:41:40 +0800

    add more document filed to index

--- preprocess/docs.py
+++ preprocess/docs.py
@@ -21,7 +21,11 @@ class Document(object):
         self.minimum_age = kwargs['minimum_age']
         self.maximum_age = kwargs['maximum_age']
         self.healthy_volunteers = kwargs['healthy_volunteers']
-
+        self.textblock = kwargs['textblock']
+        self.mesh_term = kwargs['mesh_term']
+        self.condition = kwargs['condition']
+        self.keyword = kwargs['keyword']
+        
     def toJsonObj(self):
         '''
             Return the json object (python dict) of this topic instance which may be used in elasticsearch module.
@@ -29,7 +33,8 @@ class Document(object):
         jsonObj = {"nct_id": self.nct_id, "brief_title": self.brief_title, "official_title": self.official_title,
                 "brief_summary":self.brief_summary, "study_type":self.study_type,
                 "primary_purpose":self.primary_purpose, "gender":self.gender, "minimum_age":self.minimum_age,
-                "maximum_age":self.maximum_age, "healthy_volunteers":self.healthy_volunteers}
+                "maximum_age":self.maximum_age, "healthy_volunteers":self.healthy_volunteers, "textblock" : self.textblock,
+                "mesh_term" : self.mesh_term, "condition" : self.condition, "keyword":self.keyword}
         # jsonObj = {"nct_id": self.nct_id, "brief_title": self.brief_title, "official_title": self.official_title,
         #         "brief_summary":self.brief_summary}
         return jsonObj
@@ -45,16 +50,18 @@ def loadDoc(loadFile=__rawDocFile):
     # get important doc node
     nct_id = str(rootNode.getElementsByTagName('nct_id')[0].firstChild.nodeValue)
     
+    # get brief_title
     if len(rootNode.getElementsByTagName('brief_title')) == 0:
         brief_title = ''
     else:
         brief_title = str(rootNode.getElementsByTagName('brief_title')[0].firstChild.nodeValue)
-    
+    # get official_title
     if len(rootNode.getElementsByTagName('official_title')) == 0:
         official_title = ''
     else:
         official_title = str(rootNode.getElementsByTagName('official_title')[0].firstChild.nodeValue)
     
+    # brief_summary
     if len(rootNode.getElementsByTagName('brief_summary')) == 0:
         brief_summary = ''
     else:
@@ -95,7 +102,37 @@ def loadDoc(loadFile=__rawDocFile):
         healthy_volunteers = str(rootNode.getElementsByTagName('healthy_volunteers')[0].firstChild.nodeValue)
     # mesh_term = str(rootNode.getElementsByTagName('mesh_term')[0].firstChild)
     
+    # textblock consist of detail_description,brifsummry, critira
+    textblockList = rootNode.getElementsByTagName('textblock')
+    text = []
+    for t in textblockList:
+        text.append(t.childNodes[0].data)
+    textblock = str('\n'.join(text))
+    # detailed_description = str(rootNode.getElementsByTagName('detailed_description')[0].getElementsByTagName('textblock')[0].childNodes[0].data)
+
+
+    # mesh_term
+    mesh_term_list = rootNode.getElementsByTagName('mesh_term')
+    meshTerm = []
+    for meshterm in mesh_term_list:
+        meshTerm.append(meshterm.childNodes[0].data)
+    mesh_term = str(','.join(meshTerm))
+
+    # # condition
+    condition_list = rootNode.getElementsByTagName('condition')
+    cond = []
+    for con in condition_list:
+        cond.append(con.childNodes[0].data)
+    condition = str(','.join(cond))
+
+    # keyword ,some document may contain this field
+    keywordList = rootNode.getElementsByTagName('keyword')
+    key = []
+    for k in keywordList:
+        key.append(k.childNodes[0].data)
+    keyword = str(','.join(key))
+
     docs = Document(nct_id=nct_id, brief_title=brief_title, official_title=official_title, brief_summary=brief_summary, study_type=study_type, 
                     primary_purpose=primary_purpose, gender=gender, minimum_age=minimum_age, maximum_age=maximum_age,
-                    healthy_volunteers=healthy_volunteers)
+                    healthy_volunteers=healthy_volunteers, textblock=textblock, mesh_term=mesh_term, condition=condition, keyword=keyword)
     return docs
