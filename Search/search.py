2a53b2e655cf6cf9102ce25bec3b7000a5a00840
ZhangJiahua <zhangjiahua@ict.ac.cn>
2018-12-30 18:25:02 +0800

    add 36 attr

--- Search/search.py
+++ Search/search.py
@@ -15,6 +15,9 @@ dataDir = os.path.join(parentDir, 'data')
 docDir = os.path.join(dataDir, 'clinicaltrials_xml')
 resultDir = os.path.join(dataDir, 'result.txt')
 
+bm25Index = "clinicaltrials_bm25"
+tfidfIndex = "clinicaltrials_tfidf"
+
 # connect to es ser ver
 es = Elasticsearch([{'host':'localhost', 'port' : 9200}])
 
@@ -61,15 +64,66 @@ def queryOnOfficial_title(query):
     }
     return body
 
-def queryOnBrief_summary(query):
+# def queryOnBrief_summary(query):
+#     body = {
+#         "query" : {
+#             "match": {
+#                 "brief_summary": query
+#             }
+#         }
+#     }
+#     return body
+
+def queryOnTextblock(query):
     body = {
         "query" : {
             "match": {
-                "brief_summary": query
+                "textblock": query
             }
         }
     }
     return body
+
+def queryOnMesh_term(query):
+    body = {
+        "query" : {
+            "match": {
+                "mesh_term": query
+            }
+        }
+    }
+    return body
+
+def queryOnCondition(query):
+    body = {
+        "query" : {
+            "match": {
+                "condition": query
+            }
+        }
+    }
+    return body
+
+def queryOnKeyword(query):
+    body = {
+        "query" : {
+            "match": {
+                "keyword": query
+            }
+        }
+    }
+    return body
+
+def queryOnXXX(query, XXX):
+    body = {
+        "query" : {
+            "match": {
+                XXX: query
+            }
+        }
+    }
+    return body
+
 
 rawtopics = topics.loadRawTopics()
 
@@ -85,7 +139,7 @@ def getAllTopicScore():
         gene = ','.join(rawTopic.getGeneList())
         other = rawTopic.getOther()
         query = disease + gene + other
-        result = es.search(index='clinicaltrials',doc_type='trial', body=queryBody(query,1,1,1), explain=True)
+        result = es.search(index='clinicaltrials_bm25',doc_type='trial', body=queryBody(query,1,1,1), explain=True)
         # print(result)
         # print(result['hits'])
         with open(resultDir, 'a') as resultFile:
@@ -99,6 +153,13 @@ def getScore(whichIndex, docId, queryBody):
     score = res['explanation']["value"]
     return score
 
+def getResScore(whichIndex, queryBody):
+    resDict = {}
+    Results = es.search(index=whichIndex, doc_type='trial',body=queryBody)["hits"]["hits"]
+    for hit in Results:
+        res = {hit["_id"] : hit["_score"]}
+        resDict.update(res)
+    return resDict
 
 def subScoreList(queryTopicId, docId, whichIndex):
     sList = []
@@ -109,17 +170,26 @@ def subScoreList(queryTopicId, docId, whichIndex):
     # for disease
     sList.append(getScore(whichIndex, docId, queryOnBrief_title(disease)))
     sList.append(getScore(whichIndex, docId, queryOnOfficial_title(disease)))
-    sList.append(getScore(whichIndex, docId, queryOnBrief_summary(disease)))
+    sList.append(getScore(whichIndex, docId, queryOnTextblock(disease)))
+    sList.append(getScore(whichIndex, docId, queryOnMesh_term(disease)))
+    sList.append(getScore(whichIndex, docId, queryOnCondition(disease)))
+    sList.append(getScore(whichIndex, docId, queryOnKeyword(disease)))
 
     # for gene
     sList.append(getScore(whichIndex, docId, queryOnBrief_title(gene)))
     sList.append(getScore(whichIndex, docId, queryOnOfficial_title(gene)))
-    sList.append(getScore(whichIndex, docId, queryOnBrief_summary(gene)))
+    sList.append(getScore(whichIndex, docId, queryOnTextblock(gene)))
+    sList.append(getScore(whichIndex, docId, queryOnMesh_term(gene)))
+    sList.append(getScore(whichIndex, docId, queryOnCondition(gene)))
+    sList.append(getScore(whichIndex, docId, queryOnKeyword(gene)))
 
     # for other
     sList.append(getScore(whichIndex, docId, queryOnBrief_title(other)))
     sList.append(getScore(whichIndex, docId, queryOnOfficial_title(other)))
-    sList.append(getScore(whichIndex, docId, queryOnBrief_summary(other)))
+    sList.append(getScore(whichIndex, docId, queryOnTextblock(other)))
+    sList.append(getScore(whichIndex, docId, queryOnMesh_term(other)))
+    sList.append(getScore(whichIndex, docId, queryOnCondition(other)))
+    sList.append(getScore(whichIndex, docId, queryOnKeyword(other)))
 
     return sList
 
@@ -130,3 +200,66 @@ def getScoreList(queryTopicId, docId):
     list2 = subScoreList(id, docId, "clinicaltrials_tfidf")
     List.extend(list2)
     return List
+
+def getSubScoreDict(module, topicId, docId):
+    sList = []
+    disease = ','.join(rawtopics[topicId].getDiseaseList())
+    gene = ','.join(rawtopics[topicId].getGeneList())
+    other = rawtopics[topicId].getOther()
+    
+    docFieldList = ["brief_title", "official_title", "textblock", "mesh_term", "condition", "keyword"]
+    topicFieldList = ["disease", "gene", "other"]
+
+    resDict = []
+    resDict.append(getResScore(whichIndex=module, queryBody=queryOnBrief_title(disease)))
+    resDict.append(getResScore(whichIndex=module, queryBody=queryOnOfficial_title(disease)))
+    resDict.append(getResScore(whichIndex=module, queryBody=queryOnMesh_term(disease)))
+    resDict.append(getResScore(whichIndex=module, queryBody=queryOnMesh_term(disease)))
+    resDict.append(getResScore(whichIndex=module, queryBody=queryOnOfficial_title(disease)))
+    resDict.append(getResScore(whichIndex=module, queryBody=queryOnCondition(disease)))
+
+    resDict.append(getResScore(whichIndex=module, queryBody=queryOnBrief_title(gene)))
+    resDict.append(getResScore(whichIndex=module, queryBody=queryOnOfficial_title(gene)))
+    resDict.append(getResScore(whichIndex=module, queryBody=queryOnMesh_term(gene)))
+    resDict.append(getResScore(whichIndex=module, queryBody=queryOnMesh_term(gene)))
+    resDict.append(getResScore(whichIndex=module, queryBody=queryOnOfficial_title(gene)))
+    resDict.append(getResScore(whichIndex=module, queryBody=queryOnCondition(gene)))
+
+    resDict.append(getResScore(whichIndex=module, queryBody=queryOnBrief_title(other)))
+    resDict.append(getResScore(whichIndex=module, queryBody=queryOnOfficial_title(other)))
+    resDict.append(getResScore(whichIndex=module, queryBody=queryOnMesh_term(other)))
+    resDict.append(getResScore(whichIndex=module, queryBody=queryOnMesh_term(other)))
+    resDict.append(getResScore(whichIndex=module, queryBody=queryOnOfficial_title(other)))
+    resDict.append(getResScore(whichIndex=module, queryBody=queryOnCondition(other)))
+    
+    for i in range(len(resDict)):
+        if docId in resDict[i].keys():
+            sList.append(resDict[docId])
+        else:
+            if i < 6:
+                tNumber = 0
+            elif i < 12:
+                tNumber = 1
+            else:
+                tNumber = 2
+            s = getScore(whichIndex=module, docId=docId, queryBody=queryOnXXX(topicFieldList[tNumber],docFieldList[i % 6]))
+            sList.append(s)
+            
+    return sList
+
+def getScoreDict(topicId, docIdList):
+    resDict = {}
+    for docId in docIdList:
+        list1 = getSubScoreDict(bm25Index, topicId, docId)
+        list2 = getSubScoreDict(tfidfIndex, topicId, docId)
+        list1.extend(list2)
+        resDict.update({docId:list1})
+    return resDict
+
+# def test(topicid, docID):
+#     disease = ','.join(rawtopics[topicid].getDiseaseList())
+#     # print(getRes("clinicaltrials_bm25", queryOnTextblock(disease))["_score"])
+#     print(getScore("clinicaltrials_bm25", docID, queryOnTextblock(disease)))
+# # 'NCT00002575', '_score': 9.86981
+# # 'NCT00003648', '_score': 9.649702,
+# test(1, "NCT00003648")
